278829aa980bf36a3b7ffaae5379cc9b32579a5c
Godday-Abuede <56562996+Godday-Abuede@users.noreply.github.com>
2019-11-10 00:54:23 -0500

    msg

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,42 +2,5 @@
 import os
 import csv
 
-# The path for the filename  
-PyBank_csv = os.path.join("budget_data.csv")
-
-
-# Calculate the change in Profit/Losses and save the result in a new column in 
-# dataframe named Change. 
-PyBank_csv[int('Change')] = PyBank_csv['Profit/Losses"'].diff()
-
-# Financial Analysis
-month = len(PyBank_csv["Date"])                 #The total number of months included in the dataset
-total = PyBank_csv['Profit/Losses'].sum()       # The net total amount of "Profit/Losses" over the entire period
-average = PyBank_csv.mean()                       # The average of the changes in "Profit/Losses" over the entire period
-inc = PyBank_csv.max()                        # The greatest increase in profits (date and amount) over the entire period
-dec = PyBank_csv.min()                        # The greatest decrease in losses (date and amount) over the entire period
-
-# Print the analysis to the terminal 
-print('Financial Analysis')
-print('-'*len('Financial Analysis'))
-print(f'Total  Months: {month}')
-print(f"Total: {total}")
-print(f"Average Change: ${round(average[1],2)}")
-print(f'Greatest Increase in Profits: {inc[0]} (${int(inc[2])})')
-print(f'Greatest Decrease in Profits: {dec[0]} (${int(dec[2])})')
-
-
-# Export a text file with the results.
-f = open("output.txt", "a")
-print('Financial Analysis', file=f)
-print('-'*len('Financial Analysis'), file=f)
-print(f'Total  Months: {mth}', file=f)
-print(f"Total: {ttl}", file=f)
-print(f"Average Change: ${round(avg[1],2)}", file=f)
-print(f'Greatest Increase in Profits: {inc[0]} (${int(inc[2])})', file=f)
-print(f'Greatest Decrease in Profits: {dec[0]} (${int(dec[2])})', file=f)
-f.close()
-
-    
-
+print(dir(csv))
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  9 00:15:12 2019
+
+@author: Godday Abuede
+"""
+
+# Import the required library/ module and data
+# Make sure the data is saved in the same directory/ folder as the script file
+import pandas as pd 
+dt = pd.read_csv('election.csv') 
+dt.head()
+
+# Group votes by candidates
+cd = dt.groupby('Candidate')
+cd.head()
+cd.indices.keys()
+
+# Group votes by counties
+ct = dt.groupby('County')
+ct.head()
+
+# List of candidates 
+lc = list(cd.groups.keys())
+# List of Counties
+ls = list(ct.groups.keys())
+
+# Votes by candidates
+cd.get_group('Correy') 
+cd.get_group('Khan') 
+cd.get_group('Li') 
+cd.get_group("O'Tooley") 
+
+# Total votes cast and total number of votes won by candidates
+totalVote = len(dt)
+cVote = len(cd.get_group('Correy'))                                 # Total number of votes won by Correy
+kVote = len(cd.get_group('Khan'))                                   # Total number of votes won by Khan
+lVote = len(cd.get_group('Li'))                                     # Total number of votes won by Li
+oVote = len(cd.get_group("O'Tooley"))                               # Total number of votes won by O'Tooley
+
+# Percentage share of votes by candidates
+cShare = '{0:.3f}%'.format(round(cVote / totalVote * 100))          # Correy's vote share
+kShare = '{0:.3f}%'.format(round(kVote / totalVote * 100))          # Khan's vote share
+lShare = '{0:.3f}%'.format(round(lVote / totalVote * 100))          # Li's vote share
+oShare = '{0:.3f}%'.format(round(oVote / totalVote * 100))          # O'Tooley's vote share
+
+# Create a dataframe of the election candidates, their vote share and percentage of the total vote won
+rs = {'Candidate': list(cd.groups.keys()), 
+      'Vote': [cVote, kVote, lVote, oVote],
+      'PctVote': [cShare, kShare, lShare, oShare]}
+df = pd.DataFrame(rs, columns = ['Candidate', 'Vote', 'PctVote'])
+
+
+# Print the result to the terminal 
+print('Election Results')
+print('-'*25)
+print(f'Total  Votes: {totalVote}')
+print('-'*25)
+print(f"{df.Candidate[0]}: {df.PctVote[0]} ({df.Vote[0]})")
+print(f"{df.Candidate[1]}: {df.PctVote[1]} ({df.Vote[1]})")
+print(f"{df.Candidate[2]}: {df.PctVote[2]} ({df.Vote[2]})")
+print(f"{df.Candidate[3]}: {df.PctVote[3]} ({df.Vote[3]})")
+print('-'*25)
+print(f"Winner: {df[df['Vote']==df['Vote'].max()]['Candidate'][1]}")
+print('-'*25)
+
+
+# Export the results to a text file.
+f = open("result.txt", "a")
+print('Election Results', file=f)
+print('-'*25, file=f)
+print(f'Total  Votes: {totalVote}', file=f)
+print('-'*25, file=f)
+print(f"{df.Candidate[0]}: {df.PctVote[0]} ({df.Vote[0]})", file=f)
+print(f"{df.Candidate[1]}: {df.PctVote[1]} ({df.Vote[1]})", file=f)
+print(f"{df.Candidate[2]}: {df.PctVote[2]} ({df.Vote[2]})", file=f)
+print(f"{df.Candidate[3]}: {df.PctVote[3]} ({df.Vote[3]})", file=f)
+print('-'*25, file=f)
+print(f"Winner: {df[df['Vote']==df['Vote'].max()]['Candidate'][1]}", file=f)
+print('-'*25, file=f)
+f.close()
+
+
+
 
